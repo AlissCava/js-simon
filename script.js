bc70f4ed7ef857360e4454c0a3c4ea85ba54e9b8
AlissCava <cavalleraalissa@gmail.com>
2023-11-30 15:19:11 +0100

    pensieri inb italiano

---
+++ script.js
@@ -0,0 +1,20 @@
+// Funzione per generare un array di 5 numeri casuali compresi tra 1 e 10
+
+// Funzione per visualizzare i numeri generati nell'elemento con id 'numbers-container'
+
+// Funzione principale per avviare il gioco
+    // Genera e visualizza i numeri casuali
+    // Imposta un timer per pulire i numeri dopo 30 secondi e chiamare la funzione per l'input dell'utente
+
+// Funzione per rimuovere i numeri visualizzati
+
+// Funzione per ottenere l'input dell'utente
+    // Richiede all'utente di inserire i numeri separati da spazi attraverso un prompt
+
+    // Controlla l'input dell'utente e determina quali numeri sono stati indovinati
+
+    // Visualizza il risultato
+
+// Funzione per visualizzare il risultato dell'indovinamento dell'utente
+
+// Chiamata per iniziare il gioco quando la pagina è caricata
